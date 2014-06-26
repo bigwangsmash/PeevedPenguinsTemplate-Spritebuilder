84e14757e5bbe85c40570457d0578572d4c80c8a
Brian Wang <bigwangsmash@Brians-MacBook-Pro.local>
2014-06-26 09:31:19 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/RetryButton.h
@@ -0,0 +1,13 @@
+//
+//  RetryButton.h
+//  PeevedPenguins
+//
+//  Created by Brian Wang on 6/26/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCButton.h"
+
+@interface RetryButton : CCButton
+
+@end
